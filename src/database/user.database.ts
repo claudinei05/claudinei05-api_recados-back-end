ae2a9cec267956ab7087e2970e7a6a1f403cb678
claudinei05 <oliveiraclaudinei786@gmail.com>
2023-02-19 19:39:57 -0300

    api-errands

--- src/database/user.database.ts
+++ src/database/user.database.ts
@@ -9,6 +9,9 @@ export class UserDataBase {
   public getId(id: string) {
     return users.find((user) => user.id === id);
   }
+  public get(id: string) {
+    return users.find((user) => user.id === id);
+  }
 
   public create(usuario: UserModel) {
     return users.push(usuario);
@@ -16,13 +19,13 @@ export class UserDataBase {
   // public getCpf(cpf: string) {
   //   return users.find((user) => user.cpf === cpf);
   // }
-  public indexUser(id: string) {
-    return users.findIndex((user) => user.id === id);
-  }
+  // public indexUser(id: string) {
+  //   return users.findIndex((user) => user.id === id);
+  // }
   public indexTransactions(id: string) {
     // return users.findIndex((transactions) => transactions.transactions === id);
   }
-  public deleteUser(index: number) {
+  public deleteErrands(index: number) {
     return users.splice(index, 1);
   }
   public delTransactions(index: number) {
