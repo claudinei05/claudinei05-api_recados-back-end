2512706d83a001e61c86b3abd86a368cea00cd3c
claudinei05 <oliveiraclaudinei786@gmail.com>
2023-03-26 19:15:01 -0300

    at para render

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-
 import { userRoutes } from "./routes/user.routes";
+
 dotenv.config();
+
 const app = express();
 app.use(express.json());
+
 app.use(cors());
 app.use("/user", userRoutes());
 
