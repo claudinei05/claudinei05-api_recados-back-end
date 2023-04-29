3359a12e61e07144fcd3ba71b092a1bc42d32f29
claudinei05 <oliveiraclaudinei786@gmail.com>
2023-04-28 22:28:36 -0300

    build

--- src/main/config/typeorm.config.ts
+++ src/main/config/typeorm.config.ts
@@ -4,8 +4,8 @@ import { databaseEnv } from "../../app/envs/database.env";
 let entities = "src/app/shared/database/entities/**/*.ts";
 let migrations = "src/app/shared/database/migrations/**/*.ts";
 if (databaseEnv.nodeEnv !== "dev") {
-  entities = "build/app/shared/database/entities/**/*.ts";
-  migrations = "src/app/shared/database/migrations/**/*.ts";
+  entities = "build/app/shared/database/entities/**/*.js";
+  migrations = "build/app/shared/database/migrations/**/*.js";
 }
 export default new DataSource({
   type: "postgres",
